2ae2f47410e0d29efedf24d2d7737f7f78532ab0
Doron Feldman <doronfeldman07@gmail.com>
2022-07-04 23:21:09 +0300

    move to react 17 root

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 // React 18
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 // React 17
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
 // React 18
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// React 17
-// ReactDOM.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
 //   <React.StrictMode>
 //     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
+//   </React.StrictMode>
 // );
+
+// React 17
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
